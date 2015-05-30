229b08c6be13a97109d71c9e7c4d972396bb012d
Gosia <malg.pawlak@gmail.com>
2015-05-30 13:20:43 +0000

    i can add

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -2,4 +2,17 @@ class DreamsController < ApplicationController
   def index
       @dreams = Dream.all
   end
+  
+  def new
+      @dream = Dream.new
+  end
+      
+  def create
+    Dream.create!(dream_params)
+    redirect_to dreams_path
+  end
+     
+  def dream_params
+    params.require(:dream).permit(:author, :content, :photo_url, :rating, :tags)
+  end
 end
